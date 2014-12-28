d3cb8b90b2960c598a334f89e69b27f1f54a74d0
Nicola Aitken <nicola@geckoboard.com>
2014-12-28 12:40:59 +0000

    vertabrae has position

--- lib/vertabrae.rb
+++ lib/vertabrae.rb
@@ -1,8 +1,9 @@
 class Vertabrae
 
-  attr_accessor :direction
+  attr_accessor :direction, :position
 
-  def initialize(direction)
+  def initialize(direction, x_coord, y_coord)
     @direction = direction
+    @position = [x_coord, y_coord]
   end
 end
--- spec/vertebrae_spec.rb
+++ spec/vertebrae_spec.rb
@@ -2,9 +2,13 @@ require 'vertabrae'
 
 describe 'vertabrae' do
 
+  let(:lumbar){Vertabrae.new(90, 4,1)}
+
   it 'has a direction when created' do 
-    lumbar = Vertabrae.new(90)
     expect(lumbar.direction).to eq(90)
   end
 
+  it 'has a position when created' do
+    expect(lumbar.position).to eq([4,1]) 
+  end
 end
